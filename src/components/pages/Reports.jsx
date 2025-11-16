ef1fb5768bde13a139ce52caf15fc3431894a212
Raph <onwujekwechibuzor@gmail.com>
2025-11-16 01:08:33 +0100

    removed fixed height

--- src/components/pages/Reports.jsx
+++ src/components/pages/Reports.jsx
@@ -16,7 +16,7 @@ const Reports = () => {
 
     return ( <div className="max-w-5xl mx-auto p-5 rounded-lg bg-white shadow-lg " id="reports">
         <h2 className="font-semibold text-lg mb-4">Reports</h2>
-        <div className="flex items-center mt-3">
+        <div className="flex items-center mt-3 m-4">
             <span className="block py-1 w-24 text-center bg-white border border-gray-300 cursor-pointer rounded-tl-md rounded-bl-md font-semibold">Categories</span>
             <span className="block py-1 w-24 text-center bg-gray-200 border border-gray-300 cursor-pointer font-semibold">Cash Flow</span>
             <span className="block py-1 w-24 text-center bg-gray-200 border border-gray-300 cursor-pointer rounded-tr-md rounded-br-md font-semibold">Trends</span>
