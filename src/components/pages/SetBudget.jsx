f5a8104cf92adc7d96b355bf3e8812608a1bfbfd
Raph <onwujekwechibuzor@gmail.com>
2025-11-18 02:49:31 +0100

    added form validation to set budget

--- src/components/pages/SetBudget.jsx
+++ src/components/pages/SetBudget.jsx
@@ -20,7 +20,8 @@ const SetBudget = () => {
        setBudgetDetails(prev=>({...prev,[e.target.name]:e.target.value}))
     }
 
-    const addCategory = ()=>{
+    const addCategory = (e)=>{
+        e.preventDefault()
         const categoryExists = budgetArr.some(item=>item['category'] == budgetDetails['category'])
         if (categoryExists) {
             setWarning(true)
@@ -49,26 +50,24 @@ const SetBudget = () => {
     <div className="max-w-5xl mx-auto p-5 rounded-lg bg-white shadow-lg scroll-mt-22" id="overview">
         <h2 className='font-bold mb-3 text-xl'>Set Budget</h2>
         <span className="text-gray-400 mb-6 block md:font-semibold">Add Categories and amounts below. Amounts must be numeric: required fields are marked</span>
-        <div className=" gap-6 grid grid-cols-1 mb-3  lg:flex lg:w-full">
+        <form onSubmit={(e)=>addCategory(e)} className=" gap-6 grid grid-cols-1 mb-3  lg:flex lg:w-full">
             <div className="lg:w-[40%]">
-                <span className="text-gray-400 mb-1 block md:font-semibold">Budget Category <span className="inline-block text-red-400 font-semibold">*</span></span>
-                <select ref={categorySelectRef} name='category' className="block p-2 font-bold border border-gray-400 cursor-pointer rounded-lg w-full" value={budgetDetails['category']} onChange={(e)=>handleCategoryChange(e)}>
-                            <optgroup>
+                <span className="text-gray-600 mb-1 block md:font-semibold">Budget Category <span className="inline-block text-red-400 font-semibold">*</span></span>
+                <select required ref={categorySelectRef} name='category' className="block p-2 font-bold border border-gray-400 cursor-pointer rounded-lg w-full" value={budgetDetails['category']} onChange={(e)=>handleCategoryChange(e)}>
                                 <option value="" disabled={true}>Choose Category</option>
                                 <option value="Rent" className="font-bold">Rent</option>
                                 <option value="Food" className="font-bold">Food</option>
                                 <option value="Utilities" className="font-bold">Utilities</option>
                                 <option value="Transport" className="font-bold">Transport</option>
                                 <option value="Health" className="font-bold">Health</option>
-                            </optgroup>
                 </select>
             </div>
             <div className="lg:w-[40%]">
-                <span className="text-gray-400 mb-1 block md:font-semibold">Amount <span className="inline-block text-red-400 font-semibold">*</span></span>
-                <input name='amount' type="number" className="block p-2 font-bold border border-gray-400 cursor-pointer rounded-lg w-full" value={budgetDetails['amount']} onChange={(e)=>handleAmountChange(e)}/>
+                <span className="text-gray-600 mb-1 block md:font-semibold">Amount <span className="inline-block text-red-400 font-semibold">*</span></span>
+                <input required name='amount' type="number" className="block p-2 font-bold border border-gray-400 cursor-pointer rounded-lg w-full" value={budgetDetails['amount']} onChange={(e)=>handleAmountChange(e)}/>
             </div>
-            <button className=" bg-gray-300 cursor-pointer p-2 font-semibold rounded-lg" onClick={()=>addCategory()}><FaPlus className='mr-2 inline-block'/>Add Category</button>
-        </div>
+            <button className=" bg-gray-300 cursor-pointer p-2 font-semibold rounded-lg" type='submit'><FaPlus className='mr-2 inline-block'/>Add Category</button>
+        </form>
         <p className={`p-2 rounded-lg flex items-center bg-gray-200 mt-6  md:font-semibold ${warning?'text-red-500':'text-gray-400'}`}><FaInfo className={`text-md inline-block ml-2 mr-2 border-2 border-gray-400 rounded-full ${warning?'border-red-500':'border-gray-400'}`}/>{warning?'Category has already been set, edit below':'Each category can only be added once'}.</p>
     </div> 
     <BudgetList setWarning={setWarning}/>
