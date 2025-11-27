95ed363170bffcba8d2b3d0be01420a80171a3bb
Raph <onwujekwechibuzor@gmail.com>
2025-11-27 22:12:08 +0100

    added dark mode to tooltip

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -33,12 +33,12 @@ const NavBar = () => {
         <FiMenu className={`text-gray-800 text-4xl  rounded-sm cursor-pointer dark:text-white  lg:hidden md:hidden ${isPanelOpen?'hidden':'block'}` } onClick={()=>setIsPanelOpen(true)}/>
         <div className="relative w-full pl-2">
         <FaSearch className="absolute cursor-pointer top-3 left-5 text-gray-500 dark:text-black" />
-        <input type="text" data-name='input' placeholder="Search transactions, categories..." className="outline-none rounded-md text-gray-900  dark:bg-gray-100 dark:placeholder:text-black p-2 pl-9 bg-white border border-gray-300 shadow-lg text-sm sm:text-md w-[84%] md:w-[60%]"
+        <input type="text" data-name='input' placeholder="Search transactions, categories..." className="outline-none rounded-md text-gray-900 placeholder:text-sm  dark:bg-gray-100 dark:placeholder:text-black p-2 pl-9 bg-white border border-gray-300 shadow-lg text-md w-[84%] md:w-[60%]"
          value={searchInput}
          onMouseEnter={(e)=>showToolTip(e)}
          onMouseLeave={hideToolTip}
          onChange={(e)=>handleSearch(e)}/>
-        <span className={`absolute block py-1 px-2 bg-gray-100 rounded-lg shadow-lg -bottom-9 text-gray-700 transition-all duration-500 ${isToolTip['input'] ? 'opacity-100' : 'opacity-0'}`}>Search transactions by date</span>
+        <span className={`absolute block py-1 px-2 bg-gray-100 dark:text-white dark:bg-gray-600 rounded-lg shadow-lg -bottom-9 text-gray-700 transition-all duration-500 ${isToolTip['input'] ? 'opacity-100' : 'opacity-0'}`}>Search transactions by date</span>
         </div>
         <div className="flex space-x-4 items-center mr-6 cursor-pointer">
             <button onClick={toggleTheme} className='cursor-pointer'>
@@ -52,7 +52,7 @@ const NavBar = () => {
             <Link to={'/manage_budget'}>
               <div data-name='manageBudget'  className="relative" onMouseEnter={(e)=>showToolTip(e)} onMouseLeave={hideToolTip}>
                 <FaChartPie  className="text-2xl"/>
-                <span className={`absolute block w-38 text-center py-1 px-2 bg-gray-100 rounded-lg shadow-lg -bottom-10 right-0 text-gray-700 transition-all duration-500 ${isToolTip['manageBudget'] ? 'opacity-100' : 'opacity-0'}`}>Manage Budgets</span>
+                <span className={`absolute block w-38 text-center py-1 px-2 bg-gray-100 dark:text-white dark:bg-gray-600 rounded-lg shadow-lg -bottom-10 right-0 text-gray-700 transition-all duration-500 ${isToolTip['manageBudget'] ? 'opacity-100' : 'opacity-0'}`}>Manage Budgets</span>
                 {exceededBudgetCount && <span className=" absolute bg-red-500 w-4 h-4 rounded-full -top-2 -right-3 text-xs text-white font-semibold flex justify-center items-center">{exceededBudgetCount}</span>}
               </div>
             </Link>
